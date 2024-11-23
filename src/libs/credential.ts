8bcff39696ed15b6a715c7a520b18ae8484aa5bc
MoneyinIRL <easwarsenthilkumar@gmail.com>
2024-11-23 17:00:31 -0500

    Finished signup module, added a few files for authentication

---
+++ src/libs/credential.ts
@@ -0,0 +1,27 @@
+import { signIn,signOut } from "next-auth/react";
+
+export async function doLogout() {
+    await signOut({ redirectTo:"/"})
+}
+
+export async function doCredentialLogin(formdata: FormData): Promise<any>{
+    const username = formdata.get("username") as string;
+    const password = formdata.get("password") as string;
+
+    if (!username || !password) {
+        throw new Error("Username or password is missing");
+    }
+    try {
+        const response = await signIn("credentials",{
+            redirect: false,
+            username,
+            password,
+
+        });
+        console.log(response)
+        return response; 
+    } catch(err:any) {
+        throw err;
+    }
+
+}
